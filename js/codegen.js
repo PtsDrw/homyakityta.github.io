b45fe8f098ad16738957f2ee2239f98579215768
PtsDrw <77636993+PtsDrw@users.noreply.github.com>
2024-08-06 23:00:26 +0300

    Add files via upload

--- js/codegen.js
+++ js/codegen.js
@@ -1,74 +1,86 @@
-const APP_TOKEN = 'd28721be-fd2d-4b45-869e-9f253b554e50';
-const PROMO_ID = '43e35910-c168-4634-ad4f-52fd764a843f';
-const EVENTS_DELAY = 20000;
-
-document.getElementById('startBtn').addEventListener('click', async () => {
-    const startBtn = document.getElementById('startBtn');
-    const progressContainer = document.getElementById('progressContainer');
-    const progressBar = document.getElementById('progressBar');
-    const progressText = document.getElementById('progressText');
-    const keyContainer = document.getElementById('keyContainer');
-    const generatedKeys = document.getElementById('generatedKeys');
-    const keyCount = parseInt(document.getElementById('keyCountSelect').value);
-
-    // Reset UI for new generation
-    progressBar.style.width = '0%';
-    progressText.innerText = 'генерация... ожидайте';
-    progressContainer.classList.remove('hidden');
-    keyContainer.classList.add('hidden');
-    generatedKeys.innerText = '';
-
-    startBtn.disabled = true;
-
-    let progress = 0;
-    const updateProgress = (increment) => {
-        progress += increment;
-        progressBar.style.width = `${progress}%`;
-        // progressText.innerText = `${progress}%`;
-    };
-
-    const generateKeyProcess = async () => {
-        const clientId = generateClientId();
-        let clientToken;
-        try {
-            clientToken = await login(clientId);
-        } catch (error) {
-            alert(`Login failed: ${error.message}`);
-            startBtn.disabled = false;
-            return;
-        }
-
-        for (let i = 0; i < 7; i++) {
-            await sleep(EVENTS_DELAY * delayRandom());
-            const hasCode = await emulateProgress(clientToken);
-            updateProgress(15 / keyCount); // Update progress incrementally
-            if (hasCode) {
-                break;
+document.getElementById('startBtn').addEventListener('click',
+    async () => {
+        const startBtn = document.getElementById('startBtn');
+        const progressContainer = document.getElementById('progressContainer');
+        const progressBar = document.getElementById('progressBar');
+        const progressText = document.getElementById('progressText');
+        const keyContainer = document.getElementById('keyContainer');
+        const generatedKeys = document.getElementById('generatedKeys');
+        const keyCount = parseInt(document.getElementById('keyCountSelect').value);
+        const gameId = parseInt(document.getElementById('gameSelect').value);
+        if (gameId === 1) {
+            APP_TOKEN = 'd28721be-fd2d-4b45-869e-9f253b554e50';
+            PROMO_ID = '43e35910-c168-4634-ad4f-52fd764a843f';
+            EVENTS_DELAY = 20000;
+        } else if (gameId === 2) {
+            APP_TOKEN = 'd1690a07-3780-4068-810f-9b5bbf2931b2';
+            PROMO_ID = 'b4170868-cef0-424f-8eb9-be0622e8e8e3';
+            EVENTS_DELAY = 20000;
+        } else if (gameId === 3) {
+            APP_TOKEN = '74ee0b5b-775e-4bee-974f-63e7f4d5bacb';
+            PROMO_ID = 'fe693b26-b342-4159-8808-15e3ff7f8767';
+            EVENTS_DELAY = 120000;
+        } else {
+            if (gameId === 4) {
+                APP_TOKEN = '82647f43-3f87-402d-88dd-09a90025313f';
+                PROMO_ID = 'c4480ac7-e178-4973-8061-9ed5b2e17954';
+                EVENTS_DELAY = 120000;
             }
         }
+        // Reset UI for new generation
+        progressBar.style.width = '0%';
+        progressText.innerText = 'генерация... ожидайте';
+        progressContainer.classList.remove('hidden');
+        keyContainer.classList.add('hidden');
+        generatedKeys.innerText = '';
 
-        try {
-            const key = await generateKey(clientToken);
-            updateProgress(60); // Finalize progress
-            progressText.innerText = 'Готово!';
-            return key;
-        } catch (error) {
-            alert(`Key generation failed: ${error.message}`);
-            return null;
-        }
-    };
+        startBtn.disabled = true;
 
-    const keys = await Promise.all(Array.from({ length: keyCount }, generateKeyProcess));
+        let progress = 0;
+        const updateProgress = (increment) => {
+            progress += increment;
+            progressBar.style.width = `${progress}%`;
+            // progressText.innerText = `${progress}%`;
+        };
 
-    generatedKeys.innerText = keys.filter(key => key).join('\n');
-    keyContainer.classList.remove('hidden');
+        const generateKeyProcess = async () => {
+            const clientId = generateClientId();
+            let clientToken;
+            try {
+                clientToken = await login(clientId);
+            } catch (error) {
+                alert(`Login failed: ${error.message}`);
+                startBtn.disabled = false;
+                return;
+            }
 
-    startBtn.disabled = false;
-});
+            for (let i = 0; i < 7; i++) {
+                await sleep(EVENTS_DELAY * delayRandom());
+                const hasCode = await emulateProgress(clientToken);
+                updateProgress(15 / keyCount); // Update progress incrementally
+                if (hasCode) {
+                    break;
+                }
+            }
 
-document.getElementById('creatorChannelBtn').addEventListener('click', () => {
-    window.location.href = 'https://dzen.ru/id/6692df520f4cdd6ded47c3ee'; // Замените на URL вашего канала
-});
+            try {
+                const key = await generateKey(clientToken);
+                updateProgress(60); // Finalize progress
+                progressText.innerText = 'Готово!';
+                return key;
+            } catch (error) {
+                alert(`Key generation failed: ${error.message}`);
+                return null;
+            }
+        };
+
+        const keys = await Promise.all(Array.from({length: keyCount}, generateKeyProcess));
+
+        generatedKeys.innerText = keys.filter(key => key).join('\n');
+        keyContainer.classList.remove('hidden');
+
+        startBtn.disabled = false;
+    });
 
 function generateClientId() {
     const timestamp = Date.now();
